dfb4aff447ede6dce262ccad33854f610867daa7
erine <teresamvouama@gmail.com>
2024-10-07 11:01:36 +0200

    b12

--- TP_js1/test.js
+++ TP_js1/test.js
@@ -21,15 +21,49 @@ function affichage(){
 function test_couleur(){
     let couleur = prompt("Donner une couleur")
     if (couleur == "rouge"){
-        document.body.style.backgroundcolor = "red"
+        document.body.style.backgroundcolor ="red"
     }
     else if(couleur == "bleu"){
-        document.body.style.backgroundcolor = "blue"
+        document.body.style.backgroundcolor ="blue"
     }
     else if(couleur == "vert"){
-        document.body.style.backgroundcolor = "green"
+        document.body.style.backgroundcolor ="green"
     }
     else{
         document.write("Couleur non comprise")
     }
 }
+function calcul_moyenne(){
+    var n1 = prompt ("Donner la première note:");
+    var n2 = prompt ("Donner la deuxième note:");
+    var n3 = prompt ("Donner la troisième note:");
+   
+    var somme = Number(n1) + Number(n2) + Number(n3);
+
+    document.write("Voici la somme :" + somme + "<br>");
+    var moyenne = somme/3;
+    document.write("Voici la moyenne :" + moyenne + "<br>");
+   
+    if(moyenne<10)
+        {  
+             document.write("REDOUBLANT");
+            document.body.style.backgroundColor ="red";
+        }
+        
+         if (moyenne>12)
+        {
+            document.write("ADIMIS MENTION ASSEZ BIEN");
+           
+        } 
+        else if (moyenne>14)
+        {
+            document.write("ADIMIS MENTION BIEN");
+        
+        } 
+        else if (moyenne>16)
+            {
+                document.write("ADIMIS TRES MENTION BIEN");
+            
+            } 
+
+    }
