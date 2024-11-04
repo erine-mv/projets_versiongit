a0d0d6c9ba7d8498fa1f32e478ec4655a0f9b602
erine <teresamvouama@gmail.com>
2024-11-04 09:22:29 +0100

    b28

--- TP_js2/calc.js
+++ TP_js2/calc.js
@@ -33,3 +33,26 @@ function multiplication(){
     t3.value = c;
     
 }
+
+function permute(){
+    var v3 = "";
+    a = document.getElementById("t1").value;
+    b = document.getElementById("t2").value;
+    v3 = a;
+    a = b;
+    b = v3;
+
+    t1.value = a;
+    t2;value = b;
+
+}
+
+function parite(){
+    var v;
+    v = Numner(document.getElementById("t3").value);
+    if(v%2==0)
+    t4.value = ("Pair")
+    else
+    t4.value = ("Impair")
+}
+
