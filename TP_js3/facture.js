ae92554dc5277a840daf025d0c081b4869963af9
erine <teresamvouama@gmail.com>
2024-11-04 11:04:45 +0100

    b31

---
+++ TP_js3/facture.js
@@ -0,0 +1,8 @@
+function calculTotal(){
+    var a = document.getElementById("t2").value;
+    var b = document.getElementById("t3").value;
+    var c = Number(a) * Number(b);
+
+    t4.value = c;
+    
+}
